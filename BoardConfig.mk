ecfd12774b913a767430a7f2ae49a878260bd392
Jason H Weitzel <jsnweitzel@gmail.com>
2015-12-29 21:35:08 -0500

    track francos kernel 	modified:   BoardConfig.mk 	modified:   liquid.dependencies
    Change-Id: Id1ad7c7fd79467f572ce43ce98bc56c69b5ce09f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,8 +34,7 @@ ENABLE_CPUSETS := true
 # Inline kernel building
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
-TARGET_KERNEL_CONFIG := kylo_defconfig
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-6.0/bin
+TARGET_KERNEL_CONFIG := angler_defconfig
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
