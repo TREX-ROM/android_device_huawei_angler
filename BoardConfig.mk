41b5df0d70b763912a81d45e042ccc3fdbfc4f47
t-rex600 <fisch890@gmail.com>
2016-06-17 15:30:56 +0200

    angler:  switch Despair Kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -34,10 +34,10 @@ ENABLE_CPUSETS := true
 # Inline kernel building
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_SOURCE := kernel/huawei/angler
-TARGET_KERNEL_CONFIG := angler_defconfig
+TARGET_KERNEL_CONFIG := kylo_defconfig
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-4.9-kernel
 TARGET_USES_UNCOMPRESSED_KERNEL := true
 
 BOARD_KERNEL_BASE        := 0x00000000
